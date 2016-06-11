063ac1aadb055c26a6835afc0934dafb0cca1509
Artem Kondyukov <artemkondyukov@gmail.com>
2016-06-11 23:12:41 +0300

    Process fasta is now has the method returning an array.

--- utils/process_fasta.py
+++ utils/process_fasta.py
@@ -10,3 +10,17 @@ def process_fasta(filename):
 				output[current_key] += line.strip()
 
 	return output
+
+def as_array(filename):
+	output = []
+
+	with open(filename, "r") as f:
+		for line in f:
+			if line[0] == ">":
+				current_key = line[1:].strip()
+				output.append({current_key: ""})
+			else:
+				output[-1][current_key] += line.strip()
+
+	return output
+	
